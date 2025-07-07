32b5926fff2b8ea7964d741a8cf5cb79f6dc584b
Hackerolley <yakubyusufolaide@gmail.com>
2025-07-07 01:29:26 +0100

    can clear report

--- script.js
+++ script.js
@@ -234,4 +234,13 @@ function generatePDF() {
   };
 }
 
+document.getElementById("clearProgressBtn").addEventListener("click", () => {
+  const confirmClear = confirm("Are you sure you want to clear all students' progress for a new week?");
+  if (!confirmClear) return;
+
+  students = [];
+  saveToLocal();
+  renderStudents();
+});
+
 
